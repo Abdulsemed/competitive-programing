ff5333085729b6dd75a3a51f9e03102d46af7bd8
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2022-10-03 17:45:27 +0300

    Create 15- remove duplicates II.py

---
+++ 15- remove duplicates II.py
@@ -0,0 +1,24 @@
+beg = head
+        end = head  
+        if end is not None:
+            while end.next is not None:
+                if end.val == end.next.val:
+                    while end.next is not None and end.val == end.next.val:
+                        end = end.next
+                    while beg.next.val != end.val:
+                            beg = beg.next
+                    if end.next is None and beg.val != end.val:
+                        beg.next = None
+                    elif end.next is not None and beg.val != end.val:
+                        beg.next = end.next
+                        end = end.next
+                    elif end.next is not None:
+                        head = end.next
+                        beg =head
+                        end =head
+                    else:
+                        head = None
+                 
+                else:
+                    end = end.next
+        return head
