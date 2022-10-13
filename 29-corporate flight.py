e34683e9b15523a9de3554de3cd8fc025dea4fbe
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2022-10-13 21:12:30 +0300

    Create 29-corporate flight.py

---
+++ 29-corporate flight.py
@@ -0,0 +1,15 @@
+ def corpFlightBookings(self, bookings, n):
+        """
+        :type bookings: List[List[int]]
+        :type n: int
+        :rtype: List[int]
+        """
+        count_arr = [0] * (n + 1)
+        for index in range(len(bookings)):
+            count_arr[bookings[index][0]-1] += bookings[index][2]
+            count_arr[bookings[index][1]] -= bookings[index][2]
+        print(count_arr)
+        for index in range(n):
+            count_arr[index+1] +=count_arr[index]
+        count_arr.pop()
+        return count_arr
