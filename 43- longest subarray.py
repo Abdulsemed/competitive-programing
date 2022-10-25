d5cc243ea710246c352db95fc83919bddb620029
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2022-10-25 11:21:06 +0300

    Update 43- longest subarray.py

--- 43- longest subarray.py
+++ 43- longest subarray.py
@@ -2,16 +2,20 @@ def longestSubarray(self, nums: List[int]) -> int:
         beg =0
         end = 0
         max_size = 0
-        pos =[]
-        zeroes = 0
+        pos =-1
+        count = 0
         while end < len(nums):
-            if nums[end] == 0:
-                zeroes += 1
-                pos.append(end)
-            if zeroes > 1:
-                zeroes -= 1
-                beg = pos[0] + 1
-                pos.pop(0)
-            max_size = max(max_size, len(nums[beg:end]))
+            if nums[end] == 0 and pos == -1:
+                pos = end
+            elif nums[end]==0  :
+                count -= len(nums[beg:pos])
+                beg = pos + 1
+                pos = end
+            else:
+                count += 1
+            if pos == -1:
+                max_size = max(max_size, count-1)
+            else:
+                max_size = max(max_size, count)
             end += 1
         return max_size
