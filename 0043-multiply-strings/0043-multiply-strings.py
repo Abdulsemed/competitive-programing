6e596cba2888d4a29793c7e87bdf6bbea3709336
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2022-12-24 17:49:34 +0300

    Time: 34 ms (94.25%), Space: 14 MB (29.10%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,6 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        intNum1 = int(num1)
+        intNum2 = int(num2)
+        multiplied = str(intNum1*intNum2)
+        return multiplied
