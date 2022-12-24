d54633f4486d77b84ff4e000b7fed72a82418226
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2022-12-24 22:19:18 +0300

    Time: 73 ms (89.61%), Space: 14.7 MB (98.32%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -0,0 +1,17 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        leftPointer = 0
+        sizeOfArr = len(arr)
+        while leftPointer < sizeOfArr:
+            if arr[leftPointer] is  0:
+                arr.insert(leftPointer,0)
+                leftPointer += 1
+                arr.pop()
+            leftPointer += 1
+        print(arr)
+                
+                
+        
