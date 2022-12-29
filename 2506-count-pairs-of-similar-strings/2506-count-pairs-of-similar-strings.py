fd72e6ccd7385ea81d74625ddb9d814ec74e0293
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2022-12-29 18:42:36 +0300

    Time: 163 ms (55.55%), Space: 13.9 MB (97.50%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -1,14 +1,24 @@
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
         count = 0
+        jumpDict = {}
         size = len(words)
-        for index in range(size-1):
-            if index+1 < size:
-                right = index+1
-                setA = set(words[index])
-            while right < size:
-                if set(words[right]) == setA:
-                    count += 1
-                right += 1
-        
+        for index in range(size):
+            strings = set(words[index])
+            sets =''.join(sorted(list(strings)))
+            if sets in jumpDict:
+                if jumpDict[sets] > 1:
+                    jumpDict[sets] -= 1
+                    count += jumpDict[sets]
+            else:
+                jumpDict[sets] = 0
+                if index + 1 < size:
+                    right = index+1
+                    while right < size:
+                        if strings == set(words[right]):
+                            jumpDict[sets] += 1
+                        right += 1
+                            
+                    count += jumpDict[sets]
+                    
         return count
