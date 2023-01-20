b5501bf1840bc9145d49141c1c83e0d9515df431
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-01-20 09:43:12 +0300

    Time: 734 ms (92.15%), Space: 45.5 MB (11.00%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
@@ -1,13 +1,20 @@
 class Solution:
     def countDistinctIntegers(self, nums: List[int]) -> int:
-        numDict = {}
-        for element in nums:
-            numDict[element] = 1
-        for element in nums:
-            val = int(str(element)[::-1])
-            numDict[val] =1
+        distinctSet = set(tuple(nums))
+        nums[:] = list(map(str,nums))
+        count = len(distinctSet)
+        if count == 1:
+            if int(nums[0][::-1]) == int(nums[0]):
+                return 1
+            else:
+                return 2
+        else:
+            for element in nums:
+                val = int(element[::-1])
+                if val not in distinctSet:
+                    distinctSet.add(val)
+                    count += 1
         
-        print(numDict)
-        return len(numDict.values())
+        return count
             
         
