e95c377278106a7aba47b3189b521672bba082ea
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-01-22 09:46:06 +0300

    Time: 36 ms (73.79%), Space: 13.8 MB (96.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,15 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        maxim = max(nums)+1
-        newArr = [0]*(maxim)
+        maxim  = max(nums)+1
+        numDict = {0:0, 1:0, 2:0}
         for element in nums:
-            newArr[element] += 1
-        val = 0
+            numDict[element] = 1 + numDict.get(element)
+        size = len(nums)
+        nums[:] = []
         for index in range(maxim):
-            for _ in range(newArr[index]):
-                nums[val] = index
-                val+=1
+            lists = [index]*(numDict[index])
+            nums.extend(lists)
         return nums
-        
-        
