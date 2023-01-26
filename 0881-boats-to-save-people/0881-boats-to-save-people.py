31f884fa811eb7dc2e95009ed7222541f759d38f
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-01-26 19:31:32 +0300

    Time: 490 ms (55.62%), Space: 20.9 MB (66.73%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        if len(people) == 1 or (sum(people) <= limit and len(people) == 2):
+            return 1
+        else:
+            people = sorted(people)
+            beg = 0
+            boats = 0
+            end = len(people)-1
+            while beg <= end:
+                if people[end] == limit:
+                    end -= 1
+                    boats += 1
+                elif beg == end:
+                    boats += 1
+                    end -= 1
+                elif people[beg] + people[end] <= limit:
+                    boats += 1
+                    beg += 1
+                    end -= 1
+                else:
+                    end -= 1
+                    boats += 1
+                    
+        return boats
