61938fdaadc35d6ee6618a807aac4a770ccd7498
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-01-26 20:48:16 +0300

    Time: 85 ms (20.46%), Space: 13.8 MB (61.16%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,17 +1,26 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        repDict = {}
+        repDict = defaultdict(list)
         repArr = []
+        repIntervals = []
         size = len(s)
         for index in range(size):
-            repDict[s[index]] = index
+            repDict[s[index]].append(index)
+        for key in repDict:
+            repIntervals.append([repDict[key][0],repDict[key][-1]])
         beg = 0
         end = 0
-        for index in range(size):
-            end = max(end, repDict[s[index]])
-            if index == end:
-                repArr.append(end - beg +1)
-                beg = end+1
+        maxSize = 0
+        size = len(repIntervals)
+        while beg < size:
+            end += 1
+            maxSize = max(maxSize, repIntervals[beg][1])
+            while end < size:
+                if maxSize < repIntervals[end][0]:
+                    break
+                maxSize = max(maxSize, repIntervals[end][1])
                 end += 1
+            repArr.append(maxSize-repIntervals[beg][0]+1)
+            beg = end
         return repArr
                 
