bd645a71a8a7a657119f539833dac086fc1edde0
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-03-02 14:51:28 +0300

    Time: 41 ms (53.10%), Space: 14 MB (23.23%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -4,19 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        temp1 = list1
-        temp2 = list2
-        newNode = ListNode(0)
-        runner = newNode
+    def compare(self,temp1,temp2,runner):
+        if not(temp1 or temp2):
+            return temp1, temp2, runner
+        temp1val = temp1.val if temp1 else 101
+        temp2val= temp2.val if temp2 else 101
+        if temp1val < temp2val:
+            runner.next = ListNode(temp1val)
+            temp1 = temp1.next
+        else:
+            runner.next = ListNode(temp2val)
+            temp2 = temp2.next
+        runner = runner.next
         while temp1 or temp2:
-            temp1val = temp1.val if temp1 else 101
-            temp2val= temp2.val if temp2 else 101
-            if temp1val < temp2val:
-                runner.next = ListNode(temp1val)
-                temp1 = temp1.next
-            else:
-                temp2 = temp2.next
-                runner.next = ListNode(temp2val)
-            runner = runner.next
-        return newNode.next
+            temp1, temp2, runner = self.compare(temp1,temp2,runner)
+        return temp1, temp2, runner
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        newHead = ListNode(None)
+        runner = newHead
+        self.compare(list1,list2,runner)
+        return newHead.next
+        
+        
