d991c36fe0f4085b71fc9fa56141f97cd9503076
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-03-07 15:25:06 +0300

    Time: 115 ms (47.06%), Space: 14.7 MB (13.07%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
@@ -9,21 +9,19 @@ class Solution:
         # maxim = max(lists)
         minm = min(lists)
         size = len(lists)
+        lists.sort()
         qury = []
         for query in queries:
             dict1 = Counter(list(query))
             val = sorted(dict1.items())
             qury.append(val[0][1])
-        lists.sort()
-        for query in qury:
-            if query < minm:
-                answer.append(size)
-            else:
-                index = 0
-                while index < size:
-                    if lists[index] > query:
-                        break
-                    index += 1
-                answer.append(size-index)
-        
+            left = 0
+            right = size-1
+            while left <= right:
+                mid = left + (right-left)//2
+                if lists[mid] <= val[0][1]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            answer.append(size-left)
         return answer
