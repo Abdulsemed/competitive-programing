8f84f57ac81cb1bac47bb47a94416d068752e1a0
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-03-08 15:46:57 +0300

    Time: 68 ms (97.79%), Space: 15.3 MB (10.55%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -8,31 +8,22 @@ class Solution:
     def __init__(self):
         self.bool = False
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        self.subroot(root, subRoot)
-        return self.bool
-    
-    def subroot(self,root,sbroot):
-        if self.bool or not root:
-            return None 
-        elif root.val == sbroot.val:
-            self.bool = self.traversal(root,sbroot,True)
-        self.subroot(root.left, sbroot)
-        self.subroot(root.right,sbroot)
+        lists = []
+        anotherOne = []
+        self.subroot(subRoot,anotherOne)
+        self.subroot(root,lists)
+        if len(anotherOne)> len(lists): return False
+        if ''.join(anotherOne) in ''.join(lists):
+            return True
+        return False
         
-    def traversal(self,root1,root2,flag):
-        if root1 and not root2:
-            flag = False
-            return flag
-        elif not root1 and root2:
-            flag = False
-            return flag
-        elif not root1 and not root2:
-            return flag
-        if root1.val == root2.val and flag:
-            print(root1.val , root2.val)
-            flag = self.traversal(root1.left, root2.left,flag)
-            flag = self.traversal(root1.right, root2.right,flag)
-        elif root1.val != root2.val:
-            flag = False
-        return flag
+    
+    def subroot(self,root,lists):
+        if not root:
+            lists.append("#")
+            return
+        lists.append("@"+str(root.val))
+        self.subroot(root.left,lists)
+        self.subroot(root.right,lists)
+            
         
