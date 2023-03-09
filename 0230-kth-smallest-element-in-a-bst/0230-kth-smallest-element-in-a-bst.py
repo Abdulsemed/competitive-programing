95fd89d7bdfbf19cbfc5296c0c1fbf8bfd8e7622
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-03-09 20:03:09 +0300

    Time: 46 ms (92.29%), Space: 17.9 MB (82.09%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,13 +6,21 @@
 #         self.right = right
 class Solution:
     def __init__(self):
-        self.minim = []
+        self.minim = 0
+        self.val = 0
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.val = k
         self.traverse(root)
-        return self.minim[k-1]
+        return self.minim
     def traverse(self,root):
-        if root == None:
-            return 
-        self.traverse(root.left)
-        self.minim.append(root.val)
-        self.traverse(root.right)
+        if not root:
+            return
+        elif self.minim:
+            return
+        else:
+            self.traverse(root.left)
+            self.val -= 1
+            if not self.minim and self.val == 0:
+                self.minim = root.val
+                return
+            self.traverse(root.right)
