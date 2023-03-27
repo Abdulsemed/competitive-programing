333bfdef6ac97d10cd8ba9806bee2fe1187fe200
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-03-27 11:45:02 +0300

    Time: 96 ms (5.23%), Space: 13.9 MB (71.31%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def __init__(self):
+        self.valid =  []
+        self.size = 0
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        self.size = len(s)
+        self.validate(0,s,[])
+        return self.valid
+    
+    def validate(self, index,s,path):
+        if index >= self.size:
+            if len(path) == 4:
+                self.valid.append('.'.join(path))
+        for idx in range(index, self.size):
+            val = s[index:idx+1]
+            if str(int(val)) == val and idx - index < 3 and int(val) <= 255:
+                path.append(val)
+                self.validate(idx+1,s,path[:])
+                path.pop()
+            elif idx - index >= 3:
+                return
