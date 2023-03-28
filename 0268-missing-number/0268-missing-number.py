4deafbce96d33d64b1ff1779fb0baa4e65c9f73b
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-03-28 13:55:47 +0300

    Time: 156 ms (26.02%), Space: 15.2 MB (66.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        maximum = len(nums)
-        Sum = sum(nums)
-        expected = (maximum*(maximum+1))//2
-        missing = expected - Sum
-        return missing
-            
+        index = 0
+        size = len(nums)
+        nums.append(-1)
+        size += 1
+        while index < size:
+            pos = nums[index]
+            if nums[index] == -1:
+                index += 1
+            elif pos != index:
+                nums[pos], nums[index] = nums[index], nums[pos]
+            else:
+                index += 1
+        for index in range(size):
+            if nums[index] == -1:
+                return index
