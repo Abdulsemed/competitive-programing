7fc7d80b867567336477abe4894cb477a2ea2ddb
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-03-31 11:15:10 +0300

    Time: 27 ms (88.23%), Space: 13.8 MB (39.83%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,14 +1,7 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        ans = []
-        while num:
-            rem = num % 2
-            if rem:
-                ans.append("0")
-            else:
-                ans.append("1")
-            num = num//2
-        ans.append("b")
-        ans.append("0")
-        val = int(''.join(ans[::-1]),2)
-        return val
+        length = len(bin(num))-2
+        val = ["0","b"]
+        val.extend(["1"]*length)
+        val = int(''.join(val),2)
+        return val^num
