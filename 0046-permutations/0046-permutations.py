41c08bbcc95ecd7084750522a81a17d446eb919b
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-01 17:49:12 +0300

    Time: 50 ms (10.87%), Space: 14 MB (76.55%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,7 +1,7 @@
 class Solution:
     def __init__(self):
         self.answer = []
-        self.visited = set()
+        self.visited = 0
     def permute(self, nums: List[int]) -> List[List[int]]:
         self.order(nums,[])
         return self.answer
@@ -11,11 +11,15 @@ class Solution:
             self.answer.append(path)
             return
         for idx in range(len(nums)):
-            if nums[idx] not in self.visited:
+            search = abs(nums[idx])
+            if nums[idx] <= 0:
+                search += 11
+            val = 1<<search
+            if  self.visited & val == 0:
                 path.append(nums[idx])
-                self.visited.add(nums[idx])
+                self.visited += 2**search
                 self.order(nums,path[:])
                 path.pop()
-                self.visited.remove(nums[idx])
+                self.visited -= 2**search
                 
         
