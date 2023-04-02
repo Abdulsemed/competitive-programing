dfd8e4a4771dcba054b525f17f8e03ad801f2d1c
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-02 15:51:07 +0300

    Time: 4498 ms (13.25%), Space: 17.5 MB (37.55%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -2,9 +2,14 @@ class Solution:
     def maxProduct(self, words: List[str]) -> int:
         arr = []
         for index in range(len(words)):
-            val = set(words[index])
-            arr.append((len(words[index]),val))
-            words[index] = tuple(val)
+            mask = 0
+            size = len(words[index])
+            words[index] = tuple(set(words[index]))
+            for val in range(len(words[index])):
+                order = ord(words[index][val])- ord("a")
+                check = 1<<order
+                mask += 2**order
+            arr.append((size, mask))
         maxim = 0
         for index in range(len(words)):
             for val in range(len(words)):
@@ -12,7 +17,9 @@ class Solution:
                 if val == index:
                     continue
                 for j in range(len(words[val])):
-                    if words[val][j] in arr[index][1]:
+                    order = ord(words[val][j]) - ord("a")
+                    check = 1<<order
+                    if check & arr[index][1]:
                         flag = False
                         break
                 if flag:
