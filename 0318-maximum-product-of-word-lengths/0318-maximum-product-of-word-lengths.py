32d5571f9636a895402e915a6990469620bd4b6e
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-02 15:54:45 +0300

    Time: 4797 ms (12.66%), Space: 17.3 MB (48.81%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -4,12 +4,16 @@ class Solution:
         for index in range(len(words)):
             mask = 0
             size = len(words[index])
-            words[index] = tuple(set(words[index]))
-            for val in range(len(words[index])):
+            newWord = []
+            for val in range(size):
                 order = ord(words[index][val])- ord("a")
                 check = 1<<order
+                if check & mask != 0:
+                    continue
                 mask += 2**order
+                newWord.append(words[index][val])
             arr.append((size, mask))
+            words[index] = newWord
         maxim = 0
         for index in range(len(words)):
             for val in range(len(words)):
