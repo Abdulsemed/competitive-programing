fe3497573d93cba24bfba3fe5b44de840febfec3
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-05 16:47:56 +0300

    Time: 41 ms (21.84%), Space: 14 MB (76.67%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,15 @@
 class Solution:
-    def __init__(self):
-        self.lists =[[]]
-        self.size = 0
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.size = len(nums)
-        self.setter(0,nums,[])
-        return self.lists
-    def setter(self,index, nums,path):
-        if index >= self.size:
-            return
-        
-        for j in range(index,self.size):
-            path.append(nums[j])
-            self.setter(j+1,nums,path[:])
-            self.lists.append(path.copy())
-            path.pop()
-        return
+        size = 2**len(nums)
+        lists = []
+        for index in range(size):
+            curr = []
+            idx = 0
+            while index:
+                if index & 1 == 1:
+                    curr.append(nums[idx])
+                idx += 1
+                index >>= 1
+            lists.append(curr)
+        return lists
+                    
