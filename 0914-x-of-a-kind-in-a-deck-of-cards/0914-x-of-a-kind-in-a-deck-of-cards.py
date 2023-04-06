f3919a46539a2b57d84790db05fb8244ea48d6a2
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-06 17:18:51 +0300

    Time: 128 ms (93.66%), Space: 14.3 MB (22.46%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -8,7 +8,20 @@ class Solution:
         
         for num in deck:
             dicts[num] = 1 + dicts.get(num, 0)
-        if reduce(self.gcd, dicts.values()) > 1:
-            return True
-        return False
-        
+        ans = float("inf")
+        holder = -1
+        if len(dicts) == 1 and list(dicts.values()) == [1]:
+            return False
+        for key in dicts:
+            if holder == -1:
+                holder = key
+                continue
+            if ans == float("inf"):
+                val = self.gcd(dicts[key],dicts[holder])
+            else:
+                val = self.gcd(dicts[key], ans)
+            if val == 1:
+                return False
+            ans = min(ans, val)
+            
+        return True
