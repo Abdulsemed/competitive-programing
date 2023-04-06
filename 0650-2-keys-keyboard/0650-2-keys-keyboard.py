0e731004416c5d798e21491ddaf91319a8ccf473
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-06 12:18:51 +0300

    Time: 36 ms (68.17%), Space: 13.7 MB (95.28%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -0,0 +1,23 @@
+class Solution:
+    def gcd (self, a, b):
+        if b == 0:
+            return a
+        return self.gcd(b, a%b)
+    def minSteps(self, n: int) -> int:
+        curr = 1
+        count = 1
+        val = 1
+        flag = True
+        while val < n:
+            flag = False
+            ans = gcd(n,val)
+            if ans == curr:
+                val += curr
+                count += 1
+            else:
+                curr = ans
+                val += curr
+                count += 2  
+        if flag:
+            count = 0
+        return count
