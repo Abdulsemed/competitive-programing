f68569f84ab0a005bcbce0a5cc88762c909fe839
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-07 09:39:27 +0300

    Time: 6372 ms (26.60%), Space: 31.5 MB (24.58%) - LeetHub

---
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.py
@@ -0,0 +1,33 @@
+class Solution:
+    def sieve(self, size):
+        self.bools = []
+        
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        self.bools = [True] *(right+1)
+        self.bools[0] = self.bools[1] = False
+        idx = 2
+        while idx * idx <= right:
+            
+            if self.bools[idx]:
+                val = idx * idx
+                while val <= right:
+                    self.bools[val] = False
+                    val += idx
+                    
+            idx += 1
+        arr = []
+        for index in range(left,right+1):
+            if self.bools[index]:
+                arr.append(index)
+        l_ptr = 0
+        r_ptr = 1
+        self.bools = [-1,-1]
+        minim = float("inf")
+        while r_ptr < len(arr):
+            if arr[r_ptr] - arr[l_ptr] < minim:
+                self.bools = [arr[l_ptr], arr[r_ptr]]
+                minim = arr[r_ptr] - arr[l_ptr]
+            l_ptr += 1
+            r_ptr += 1
+        return self.bools
+        
