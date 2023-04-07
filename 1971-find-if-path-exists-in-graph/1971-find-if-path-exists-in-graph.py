b934b5e62f53d7b892e24822639d10fa87ba7e1d
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-07 18:25:04 +0300

    Time: 1930 ms (67.38%), Space: 106.2 MB (72.73%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,22 +1,18 @@
 class Solution:
-    def __init__(self):
-        self.sets = set()
-        self.graph = defaultdict(list)
-    def dfs(self, node,dest):
-        if node == dest:
-            return True
-        
-        self.sets.add(node)
-        for adj_node in self.graph[node]:
-            if adj_node not in self.sets:
-                flag = self.dfs(adj_node, dest)
-                if flag:
-                    return True
-                
-        return False
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = defaultdict(list)
         for index in range(len(edges)):
-            self.graph[edges[index][0]].append(edges[index][1])
-            self.graph[edges[index][1]].append(edges[index][0])
-            
-        return self.dfs(source,destination)
+            graph[edges[index][0]].append(edges[index][1])
+            graph[edges[index][1]].append(edges[index][0])
+        stack = [source]
+        visited = set()
+        while stack:
+            currNode = stack.pop()
+            if currNode == destination:
+                return True
+            visited.add(currNode)
+            for adj_node in graph[currNode]:
+                if adj_node not in visited:
+                    stack.append(adj_node)
+                    
+        return False
