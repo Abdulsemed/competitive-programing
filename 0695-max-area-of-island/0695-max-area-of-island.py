8279f6cf0ab65754fc68b2bcde3e5417e7df9b05
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-13 17:58:58 +0300

    Time: 149 ms (41.61%), Space: 17.8 MB (26.02%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,28 +4,30 @@ class Solution:
         self.size = 0
         self.colSize = 0
         self.visited = set()
-        self.count = 0
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         count = 0
         self.size = len(grid)
         self.colSize = len(grid[0])
         for index in range(self.size):
             for val in range(self.colSize):
-                self.count = 1
+                ones = 1
                 if grid[index][val] == 1 and (index,val) not in self.visited:
-                    self.dfs(index,val, grid)
-                    count = max(count, self.count)
+                    ones = self.dfs(index,val, grid, ones)
+                    count = max(count, ones)
         return count
     
     def inBound(self,row,col):
         return 0<= row < self.size and 0<= col < self.colSize
     
-    def dfs(self,row,col,grid):
+    def dfs(self,row,col,grid,count):
         self.visited.add((row,col))
         for row_change, col_change in self.directions:
             new_row = row_change+ row
             new_col = col_change + col
             if self.inBound(new_row, new_col) and grid[new_row][new_col] == 1 and (new_row, new_col) not in self.visited:
-                self.count += 1
-                self.dfs(new_row, new_col,grid)      
+                count += 1
+                count = self.dfs(new_row, new_col,grid, count)
+                
+        return count
+                
         
