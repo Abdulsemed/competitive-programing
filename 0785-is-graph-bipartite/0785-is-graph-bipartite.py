b2ca90e36db561a72a30c8eeac3ef2993ca1bbf7
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-14 10:53:35 +0300

    Time: 179 ms (55.50%), Space: 14.4 MB (68.86%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,30 @@
+class Solution:
+    def __init__(self):
+        self.visited =set()
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        size = len(graph)
+        arr= [0]*(size)
+        for index in range(size):
+            if index not in self.visited:
+                color  = 1
+                if arr[index] == 1:
+                    color = 2
+                bools = self.iterative(index,color,graph,arr)
+                if not bools:
+                    return False
+                
+        return True
+    def iterative(self,index,color, graph,arr):
+        self.visited.add(index)
+        arr[index] = color +1 if color == 1 else 1
+        for val in graph[index]:
+            if val not in self.visited:
+                if arr[val] != color and arr[val] != 0:
+                    return False
+                arr[val] = color
+                bools = self.iterative(val, color+1 if color == 1 else 1, graph,arr)
+                if not bools:
+                    return False
+            elif arr[val] == arr[index]:
+                return False
+        return True
