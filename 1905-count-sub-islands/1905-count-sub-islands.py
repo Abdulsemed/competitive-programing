92285415948d80a1cb35e80de305e0190c615f55
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-18 16:12:32 +0300

    Time: 3749 ms (14.16%), Space: 93.1 MB (38.99%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -0,0 +1,34 @@
+class Solution:
+    def __init__(self):
+        self.count = 0
+        self.directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        self.visited = set()
+        self.bools = True
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        for index in range(len(grid1)):
+            for val in range(len(grid1[0])):
+                self.bools = True
+                if (index,val) not in self.visited and grid2[index][val]:
+                    self.dfs(index,val,grid1,grid2)
+                    if grid1[index][val] and self.bools:
+                        print(index, val)
+                        self.count += 1
+                
+        return self.count
+    def inbound(self,row,col,grid):
+        return 0<= row < len(grid) and 0<= col < len(grid[0])
+    def dfs(self,row,col,grid1,grid2):
+        self.visited.add((row,col))
+        
+        for row_change, col_change in self.directions:
+            new_row  = row + row_change
+            new_col = col + col_change
+            if self.inbound(new_row,new_col,grid1) and (new_row,new_col) not in self.visited:
+                if grid2[new_row][new_col]:
+                    if self.bools and grid1[new_row][new_col]:
+                        self.bools = True
+                    else:
+                        self.bools = False
+                    self.dfs(new_row,new_col,grid1,grid2)
+                
+        
