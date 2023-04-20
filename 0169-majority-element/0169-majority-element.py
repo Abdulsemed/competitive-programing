89e830ae3c4f5f7a14b9b4176b3cdc710b532381
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-20 17:22:14 +0300

    Time: 168 ms (54.68%), Space: 15.5 MB (73.65%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dicts = {}
-        for element in nums:
-            dicts[element] = 1 + dicts.get(element,0)
+        dicts = Counter(nums)
         size = len(nums)/2
         for key in dicts:
             if dicts[key] > size:
