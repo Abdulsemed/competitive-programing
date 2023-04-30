1dadb21a9ac020e95ba0e55769235faf38570c7f
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-30 13:32:13 +0300

    Time: 6777 ms (5.02%), Space: 17.7 MB (5.63%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -1,39 +1,30 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int: 
         level = 0
-        queue = deque([["0","0","0","0"]])
+        queue = deque(["0000"])
         visited = {"0000"}
-        targ = list(target)
         if "0000" in deadends: 
             return -1
         elif "0000" == target:
             return level
-        flag = False
         while queue:
             length = len(queue)
             for _ in range(length):
                 curr = queue.popleft()
-                if curr == targ:
-                    flag = True
-                    queue = []
-                    break
+                if curr == target:
+                    return level
                 for index in range(4):
-                    if flag: break
-                    prev = curr[index]
                     for val in [1,-1]:
-                        curr[index] = prev
-                        if int(curr[index])+val < 0:
-                            curr[index] = "9"
-                        elif int(curr[index])+val > 9:
-                            curr[index] = "0"
+                        num = int(curr[index])+val
+                        if num < 0:
+                            holder  = "9"
+                        elif num > 9:
+                            holder = "0"
                         else:
-                            curr[index] = str(int(curr[index])+val)   
-                        string = ''.join(curr)
+                            holder = str(num)   
+                        string = curr[:index]+holder+curr[index+1:]
                         if string not in deadends and string not in visited:
                             visited.add(string)
-                            queue.append(curr[:])
-                    curr[index] = prev
-            if not flag:level +=1
-        if flag:
-            return level   
+                            queue.append(string)
+            level +=1
         return -1
