14250cf41f5321ea0489afdb3c30c5e9ab47ebac
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-04-30 12:37:31 +0300

    Time: 6995 ms (5.02%), Space: 17.8 MB (5.63%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -3,7 +3,7 @@ class Solution:
         level = 0
         queue = deque([["0","0","0","0"]])
         visited = {"0000"}
-        curr = [element for element in target]
+        targ = list(target)
         if "0000" in deadends: 
             return -1
         elif "0000" == target:
@@ -12,8 +12,11 @@ class Solution:
         while queue:
             length = len(queue)
             for _ in range(length):
-                if flag: break
                 curr = queue.popleft()
+                if curr == targ:
+                    flag = True
+                    queue = []
+                    break
                 for index in range(4):
                     if flag: break
                     prev = curr[index]
@@ -26,15 +29,11 @@ class Solution:
                         else:
                             curr[index] = str(int(curr[index])+val)   
                         string = ''.join(curr)
-                        if string == target:
-                            flag = True
-                            queue = []
-                            break
                         if string not in deadends and string not in visited:
                             visited.add(string)
                             queue.append(curr[:])
                     curr[index] = prev
-            level +=1
+            if not flag:level +=1
         if flag:
             return level   
         return -1
