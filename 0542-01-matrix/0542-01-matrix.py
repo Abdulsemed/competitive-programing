121c9630c5ddd810f8ce9275479c02a49c14aad4
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-01 10:05:19 +0300

    Time: 830 ms (16.58%), Space: 20.8 MB (5.09%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -16,20 +16,22 @@ class Solution:
                 ans[index].append(0)
         directions = [(0,1),(1,0),(0,-1),(-1,0)]        
         while queue:
-            row,col = queue.popleft()
-            for row_c,col_c in directions:
-                new_r = row+row_c
-                new_c = col+col_c
-                if self.inbound(new_r,new_c,mat) and (new_r,new_c) not in visited:
-                    if mat[new_r][new_c] == 0 or (mat[new_r][new_c] and ans[new_r][new_c]):
-                        queue.append((new_r,new_c))
-                        visited.add((new_r,new_c))
-                    elif ans[new_r][new_c] == 0:
-                        ans[new_r][new_c] = ans[row][col]+1
-                        queue.append((new_r,new_c))
-                        visited.add((new_r,new_c))
-                    else:
-                        ans[new_r][new_c] = min(ans[new_r][new_c],ans[row][col]+1)
-                     
+            length = len(queue)
+            for _ in range(length):
+                row,col = queue.popleft()
+                for row_c,col_c in directions:
+                    new_r = row+row_c
+                    new_c = col+col_c
+                    if self.inbound(new_r,new_c,mat) and (new_r,new_c) not in visited:
+                        if mat[new_r][new_c] and ans[new_r][new_c]:
+                            queue.append((new_r,new_c))
+                            visited.add((new_r,new_c))
+                        elif ans[new_r][new_c] == 0:
+                            ans[new_r][new_c] = ans[row][col]+1
+                            queue.append((new_r,new_c))
+                            visited.add((new_r,new_c))
+                        else:
+                            ans[new_r][new_c] = min(ans[new_r][new_c],ans[row][col]+1)
+
         return ans
         
