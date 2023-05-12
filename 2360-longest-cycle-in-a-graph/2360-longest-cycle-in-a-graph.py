98bd67ff428d4b48a87f8fef48a39f90a91ba163
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-12 19:16:35 +0300

    Time: 1659 ms (11.14%), Space: 129.5 MB (40.06%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -3,7 +3,7 @@ class Solution:
         self.visited = set()
         self.val = -1
         self.count = -1
-    def cycle(self,node,graph,colors):
+    def cycle(self,node,edges,colors):
         if colors[node] == 2:
             return True
         if colors[node] == 1:
@@ -12,8 +12,8 @@ class Solution:
                 self.count = 0
             return False
         colors[node] = 1
-        for child in graph[node]:
-            if not self.cycle(child,graph,colors):
+        if edges[node] != -1:
+            if not self.cycle(edges[node],edges,colors):
                 if self.val != -1:
                     self.count += 1
                 if self.val == node:
@@ -25,15 +25,12 @@ class Solution:
         graph = defaultdict(list)
         size = len(edges)
         colors = [0]*(size)
-        for index in range(len(edges)):
-            if edges[index] != -1:
-                graph[index].append(edges[index])
         count = -1
         for index in range(size):
             self.count = -1
             self.val = -1
             if index not in self.visited:
-                self.cycle(index,graph,colors)
+                self.cycle(index,edges,colors)
             self.visited.add(index)
             count = max(self.count,count)
         return count
