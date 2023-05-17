72bf336f7e5adb94c7965b9e2bf9f6bd6b5b467c
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-17 16:08:17 +0300

    Time: 2379 ms (18.02%), Space: 108.7 MB (52.99%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,18 +1,23 @@
 class Solution:
+    def __init__(self):
+        self.dicts = {}
+    def find(self,node):
+        if node != self.dicts[node][0]:
+            self.dicts[node][0] = self.find(self.dicts[node][0])[0]
+        return self.dicts[node]
+    def union(self,node1,node2):
+        rep_1 = self.find(node1)
+        rep_2 = self.find(node2)
+        if rep_1  == rep_2:
+            return
+        elif rep_1[1] > rep_2[1]:
+            self.dicts[rep_2[0]][0] = rep_1[0]
+            self.dicts[rep_1[0]][1] += rep_2[1]+1
+        else:
+            self.dicts[rep_1[0]][0] = rep_2[0]
+            self.dicts[rep_2[0]][1] += rep_1[1] +1
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
-        for index in range(len(edges)):
-            graph[edges[index][0]].append(edges[index][1])
-            graph[edges[index][1]].append(edges[index][0])
-        stack = [source]
-        visited = set()
-        while stack:
-            currNode = stack.pop()
-            if currNode == destination:
-                return True
-            visited.add(currNode)
-            for adj_node in graph[currNode]:
-                if adj_node not in visited:
-                    stack.append(adj_node)
-                    
-        return False
+        self.dicts ={i:[i,0] for i in range(n)}
+        for src,end in edges:
+            self.union(src,end)
+        return self.find(destination)[0] == self.find(source)[0]
