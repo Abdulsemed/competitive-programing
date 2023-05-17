5aef8209cdfff99c83b054aa219dc04a98df0ece
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-17 18:41:17 +0300

    Time: 76 ms (21.25%), Space: 16.8 MB (18.83%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,28 +1,28 @@
 class Solution:
     def __init__(self):
         self.dicts = {}
+        self.size = []
     def find(self,node):
-        if node == self.dicts[node][0]:
-            return self.dicts[node]
-        rep = self.find(self.dicts[node][0])
-        self.dicts[node][0] = rep[0]
-        return rep
+        if node != self.dicts[node]:
+            self.dicts[node] = self.find(self.dicts[node])
+        return self.dicts[node]
     
     def union(self,node1,node2):
         rep_1 = self.find(node1)
         rep_2 = self.find(node2)
         if rep_1 == rep_2:
             return True
-        elif rep_1[1] > rep_2[1]:
-            self.dicts[rep_2[0]][0] = rep_1[0]
-            self.dicts[rep_1[0]][1] += rep_2[1]
+        elif self.size[rep_1] > self.size[rep_2]:
+            self.dicts[rep_2] = rep_1
+            self.size[rep_1] += self.size[rep_2]
         else:
-            self.dicts[rep_1[0]][0] = rep_2[0]
-            self.dicts[rep_2[0]][1] += rep_1[1]
+            self.dicts[rep_1] = rep_2
+            self.size[rep_2] += self.size[rep_1]
 
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         ans = []
-        self.dicts = {index:[index,1] for index in range(len(edges))}
+        self.dicts = {index:index for index in range(len(edges))}
+        self.size = [1]*(len(edges))
         for src,end in edges:
             if self.union(src-1,end-1):
                 ans.append([src,end])
