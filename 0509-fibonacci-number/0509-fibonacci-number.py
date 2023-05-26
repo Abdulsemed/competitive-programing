938ce22ee0c39e5bce4f7c80e96f42697f935024
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-26 18:14:19 +0300

    Time: 52 ms (38.01%), Space: 16.3 MB (5.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,13 @@
 class Solution:
+    def __init__(self):
+        self.dicts = {}
     def solve(self,n,dicts):
         if n== 1:
             return 1
         if n == 0:
             return 0
-        if n in dicts:
-            return dicts[n]
-        dicts[n] = self.fib(n-1) + self.fib(n-2)
+        if n not in dicts:
+            dicts[n] = self.solve(n-1, dicts) + self.solve(n-2, dicts)
         return dicts[n]
     
     def fib(self, n: int) -> int:
