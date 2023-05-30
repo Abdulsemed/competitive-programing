f1afd50d6a66c463238f241ff63f80893eb97146
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-30 18:38:49 +0300

    Time: 1756 ms (19.29%), Space: 28.2 MB (20.35%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,20 +1,22 @@
 class Solution:
+    def __init__(self):
+        self.dicts = {}
+    def solve(self, amount, coins):
+        if amount== 0:
+            return 0
+        if amount not in self.dicts:
+            minim = float("inf")
+            for element in coins:
+                val= amount - element
+                if val >= 0:
+                    minim = min(minim, self.solve(val,coins))
+            self.dicts[amount] = minim + 1
+        return self.dicts[amount] 
+    
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0: return 0
-        
-        queue = deque([amount])
-        level = 0
-        visited = set()
-        while queue:
-            length = len(queue)
-            for _ in range(length):
-                curr = queue.popleft()
-                for val in coins:
-                    if curr - val > 0 and curr-val not in visited:
-                        visited.add(curr-val)
-                        queue.append(curr-val)
-                    elif curr - val == 0:
-                        return level + 1
-            level += 1
-        
-        return -1
+        val = self.solve(amount, coins)
+        if val == float("inf"):
+            return -1
+        return val
+            
+    
