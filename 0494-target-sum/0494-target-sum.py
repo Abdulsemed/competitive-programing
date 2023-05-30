e5f93af42b3b9fb39d33d52e5797c1b2ff7ad771
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-30 15:44:16 +0300

    Time: 523 ms (10.37%), Space: 17.2 MB (57.24%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def __init__(self):
+        self.count = 0
+        self.dicts = defaultdict(int)
+    def solve(self,index, sums,target,nums):
+        if index>= len(nums):
+            if sums == target:
+                return 1
+            return 0
+        if (index, sums) not in self.dicts:
+            self.dicts[(index,sums)] += self.solve(index+1,sums+nums[index],target,nums)
+            self.dicts[(index,sums)] += self.solve(index+1,sums-nums[index],target,nums)
+        return self.dicts[(index,sums)]
+        
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        self.solve(0,0,target,nums)
+        return self.dicts[(0,0)]
