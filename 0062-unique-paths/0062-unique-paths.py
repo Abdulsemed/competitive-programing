e5c23f14c43ee915196c00573abb303c26d782ee
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-31 19:21:53 +0300

    Time: 42 ms (49.35%), Space: 16.5 MB (6.33%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -9,3 +9,15 @@ class Solution:
                 val2 = dp[row+1][col] if row +1 < m else 0
                 dp[row][col] = val1 + val2       
         return dp[0][0]
+    def unique(self, m: int, n: int) -> int:
+        dp = [ [0 for _ in range(n)] for _ in range(m)]
+        dp[0][0] = 1
+        for row in range(n):
+            for col in range(m):
+                if row == col== 0:
+                    continue
+                val1 = dp[col][row-1] if row -1 > -1 else 0
+                val2 = dp[col-1][row] if col - 1 > -1 else 0 
+                dp[col][row] = val1 + val2
+
+        return dp[m-1][n-1]
