da78bad47da0ead780bf2ed5253deee5d237749e
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-31 18:09:18 +0300

    Time: 46 ms (52.86%), Space: 16.3 MB (30.86%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,15 +1,7 @@
 class Solution:
-    def __init__(self):
-        self.dicts = {}
-    def solve(self,n,dicts):
-        if n== 1:
-            return 1
-        if n == 0:
-            return 0
-        if n not in dicts:
-            dicts[n] = self.solve(n-1, dicts) + self.solve(n-2, dicts)
-        return dicts[n]
-    
     def fib(self, n: int) -> int:
-        dicts = {}
-        return self.solve(n,dicts)
+        dp = [0,1]
+        for index in range(2,n+1):
+            dp.append(dp[index-1]+dp[index-2])
+        return dp[n]
+        
