1d4f0d3366bb11d1f5cab2dbc9c4e5ac68baf94d
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-05-31 19:18:06 +0300

    Time: 38 ms (62.22%), Space: 16.3 MB (21.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,19 +1,11 @@
 class Solution:
-    def __init__(self):
-        self.dicts = defaultdict(int)
-        self.directions = [(0,1),(1,0)]
-    def inbound(self,row,col,m,n):
-        return 0<= row < m and 0<= col <n
-    def solve(self,row,col,m,n):
-        if row == m-1 and col == n-1:
-            return 1
-        if (row,col) not in self.dicts:
-            for r,c in self.directions:
-                new_r = row + r
-                new_c = col + c 
-                if self.inbound(new_r, new_c,m,n):
-                    self.dicts[(row,col)] += self.solve(new_r,new_c,m,n)
-        return self.dicts[(row,col)]           
-        
     def uniquePaths(self, m: int, n: int) -> int:
-        return self.solve(0,0,m,n)
+        dp = [ [0 for _ in range(n)] for _ in range(m)]
+        dp[m-1][n-1] = 1    
+        for row in range(m-1,-1,-1):
+            for col in range(n-1,-1,-1):
+                if row == m-1 and col == n-1: continue
+                val1 = dp[row][col+1] if col +1  < n else 0 
+                val2 = dp[row+1][col] if row +1 < m else 0
+                dp[row][col] = val1 + val2       
+        return dp[0][0]
