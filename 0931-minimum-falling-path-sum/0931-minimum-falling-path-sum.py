d33be3ec124eb5e019478392c6acaa4082a38449
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-07-15 19:39:07 +0300

    Time: 270 ms (5.06%), Space: 18.3 MB (17.07%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def __init__(self):
+        self.direction = [(1,-1),(1,0),(1,1)]
+        self.memo = {}
+    def inbound(self, row,col,n):
+        return 0 <= row < n and 0<= col < n
+    
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        minim = float("inf")
+        for index in range(len(matrix)):
+            minim = min(minim,self.dfs(0,index,matrix))
+            
+        return minim
+    def dfs(self, row,col,matrix):
+        if row == len(matrix)-1:
+            return matrix[row][col]
+        if (row,col) in self.memo:
+            return self.memo[(row,col)]
+        curr = float("inf")
+        for _r, _c in self.direction:
+            new_r = row + _r
+            new_c = col + _c
+            if self.inbound(new_r,new_c,len(matrix)):
+                curr = min(curr,self.dfs(new_r,new_c,matrix))
+        self.memo[(row,col)] = curr + matrix[row][col]
+        return self.memo[(row,col)]
+                
