6e7d3268e515a3e13d9ea63372bd1178928d625a
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-07-21 19:58:56 +0300

    Time: 42 ms (93.77%), Space: 16.2 MB (99.49%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -2,30 +2,26 @@ class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         ans = 1
         flag = True
-        for val in range(len(nums)):
-            count = 1
-            last = nums[val]
-            for index in range(val+1,len(nums)):
-                if flag and nums[index] > last:
-                    count += 1
-                    flag = False
-                elif not flag and nums[index] < last:
-                    flag = True
-                    count += 1
-                last = nums[index]
-            ans = max(ans,count)
-        
+        count = 1
+        last = nums[0]
+        for index in range(1,len(nums)):
+            if flag and nums[index] > last:
+                count += 1
+                flag = False
+            elif not flag and nums[index] < last:
+                flag = True
+                count += 1
+            last = nums[index]
+        ans = max(ans,count)
         flag = True
-        for val in range(len(nums)):
-            count = 1
-            last = nums[val]
-            for index in range(val+1,len(nums)):
-                if flag and nums[index] < last:
-                    count += 1
-                    flag = False
-                elif not flag and nums[index] > last:
-                    count += 1
-                    flag = True
-                last = nums[index]
-            ans = max(ans, count)
-        return ans
+        last = nums[0]
+        count = 1
+        for index in range(1,len(nums)):
+            if flag and nums[index] < last:
+                count += 1
+                flag = False
+            elif not flag and nums[index] > last:
+                count += 1
+                flag = True
+            last = nums[index]
+        return max(ans, count)
