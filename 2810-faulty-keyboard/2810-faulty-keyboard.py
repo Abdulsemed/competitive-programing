84d904e86fdac8e7ce944f3d7d78ebee21cf8e15
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-09-04 22:21:31 +0300

    Time: 57 ms (16.91%), Space: 16.3 MB (35.35%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.py
+++ 2810-faulty-keyboard/2810-faulty-keyboard.py
@@ -1,9 +1,15 @@
 class Solution:
     def finalString(self, s: str) -> str:
-        ans = []
+        ans = deque()
+        flag = False
         for element in s:
             if element == "i":
-                ans = ans[::-1]
-                continue
-            ans.append(element)
-        return ("").join(ans)
+                flag = not flag
+            elif flag:
+                ans.appendleft(element)
+            else:
+                ans.append(element)
+        result = ("").join(ans)
+        if flag:
+            result = result[::-1]
+        return result
