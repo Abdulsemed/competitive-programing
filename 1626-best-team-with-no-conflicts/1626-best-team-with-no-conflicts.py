9a61cd4707be51a904be59e1cd7ec74032f0c29a
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-02 11:03:08 +0300

    Time: 2011 ms (17.84%), Space: 16.7 MB (37.02%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,26 +1,18 @@
 class Solution:
-    def __init__(self):
-        self.dp = []
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        arr = []
-        self.dp = [[-1 for _ in range(len(scores))] for _ in range(len(scores))]
         for index in range(len(scores)):
             scores[index] = [ages[index], scores[index]]
         scores.sort(key = lambda x:(x[0],x[1]), reverse = True)
-        return self.dfs(-1,0,scores)
-    def dfs(self,curr,index,arr):
-        if index >= len(arr):
-            return 0
-        if self.dp[curr][index] != -1:
-            return self.dp[curr][index]
-        val1=val2= 0  
-        val = 0
-        minim = curr
-        if curr == -1 or arr[curr][1] >= arr[index][1]:
-            val += arr[index][1]
-            minim = index
-        val1 = self.dfs(minim,index+1,arr) + val
-        val2 = self.dfs(curr,index+1,arr)
-        self.dp[curr][index] = max(val1,val2)
-        return self.dp[curr][index]
-            
+        dp = deepcopy(scores)
+        ans = scores[len(scores)-1][1]
+        for index in range(len(scores)-2,-1,-1):
+            for val in range(index+1,len(scores)):
+                curr = 0
+                
+                if scores[index][1] >= scores[val][1] or scores[index][0] == scores[val][0]:
+                    curr += dp[val][1]
+                if scores[index][1] + curr > dp[index][1]:
+                    dp[index][1] = scores[index][1] + curr
+                    
+            ans = max(ans, dp[index][1]) 
+        return ans
