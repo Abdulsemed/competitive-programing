fa093a9aee0c40921c401fbfc1ff4b0b94c7c307
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-08 15:30:44 +0300

    Time: 568 ms (33.40%), Space: 37.3 MB (9.10%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,24 +6,22 @@
 #         self.right = right
 class Solution:
     def __init__(self):
-        self.pathDict = {0:1}
         self.currSum = 0
         self.count = 0
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.traverse(root, targetSum)
+        pathDict = {0:1}
+        self.traverse(root, targetSum, pathDict)
         return self.count
-    def traverse(self, root, target):
+    def traverse(self, root, target, pathDict):
         if not root:
             return 
         self.currSum += root.val
-        if self.currSum - target in self.pathDict:
-            self.count += self.pathDict[self.currSum-target]
-        self.pathDict[self.currSum] = 1 + self.pathDict.get(self.currSum,0)
-        self.traverse(root.left, target)
-        self.traverse(root.right, target)
-        if self.pathDict[self.currSum] > 1:
-            self.pathDict[self.currSum] -= 1
-        else:
-            del self.pathDict[self.currSum]
+        if self.currSum - target in pathDict:
+            self.count += pathDict[self.currSum-target]
+        
+        curr =  deepcopy(pathDict)
+        curr[self.currSum] = 1 + curr.get(self.currSum,0)
+        self.traverse(root.left, target, curr)
+        self.traverse(root.right, target, curr)
         self.currSum -= root.val
         
