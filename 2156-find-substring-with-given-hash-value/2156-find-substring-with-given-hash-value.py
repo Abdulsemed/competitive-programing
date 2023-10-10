a956a4db6785617a4adedd7705d1f90fd426eb98
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-10 22:32:15 +0300

    Time: 4153 ms (18.75%), Space: 797.9 MB (6.25%) - LeetHub

--- 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
+++ 2156-find-substring-with-given-hash-value/2156-find-substring-with-given-hash-value.py
@@ -1,13 +1,19 @@
 class Solution:
     def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
+        
         curr = 0
         left = 0
         right = k-1
         powers = [1] *(k)
+        raises= []
         for index in range(1,k):
             powers[index] = powers[index-1] * power
+            
         for index in range(k):
             curr += (ord(s[index]) -96) *(powers[index])
+            
+        for index in range(1,28):
+            raises.append(powers[-1] *(index))
             
         while right <= len(s):
             if curr % modulo == hashValue:
@@ -15,6 +21,6 @@ class Solution:
             else:
                 if right +1 >= len(s):
                     break
-                curr = ((curr - (ord(s[left]) -96 )) // (power)) +(ord(s[right+1] ) -96 )* powers[-1]
+                curr = ((curr - (ord(s[left]) -96 )) // (power)) +raises[ord(s[right+1] ) -97 ]
                 left += 1
                 right += 1
