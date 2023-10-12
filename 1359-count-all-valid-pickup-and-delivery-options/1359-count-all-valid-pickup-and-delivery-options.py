9feacd06e460beb177fb032b9cec5bf2201c2e79
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-12 19:43:16 +0300

    Time: 39 ms (56.08%), Space: 16.3 MB (67.42%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,9 +1,12 @@
 class Solution:
+    def multiply(self, a, b, m):
+        return ((a % m) * (b % m)) % m
     def countOrders(self, n: int) -> int:
         start = 1
+        mod = 10**9 + 7
         for index in range(2,n+1):
             val = index * 2
-            curr =  (val *(val-1))//2
-            start = start * curr
+            curr =  self.multiply(val,val-1,mod)//2
+            start = self.multiply(start, curr,mod)
             
-        return start % (10**9 + 7)
+        return start % (mod)
