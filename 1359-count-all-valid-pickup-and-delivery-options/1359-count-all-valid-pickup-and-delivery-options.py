984c7abbd2f34f1cb285a56b0edfc27405d72a0a
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-12 19:41:04 +0300

    Time: 41 ms (45.81%), Space: 16.2 MB (89.25%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -3,7 +3,7 @@ class Solution:
         start = 1
         for index in range(2,n+1):
             val = index * 2
-            curr =  (math.factorial(val) //math.factorial(val-2))//2
+            curr =  (val *(val-1))//2
             start = start * curr
             
         return start % (10**9 + 7)
