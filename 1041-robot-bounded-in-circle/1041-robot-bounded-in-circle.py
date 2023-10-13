09cdd697e0341aa306460a94a23f87d05ebfe206
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-13 21:17:06 +0300

    Time: 48 ms (7.09%), Space: 16.2 MB (84.07%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        lcount = 0
+        rcount = 0
+        lefts = {"N":"E", "E":"S","S":"W", "W":"N"}
+        rights = {"N":"W", "W":"S", "S":"E", "E":"N"}
+        go = {"N":(0,1), "E":(1,0), "W":(-1,0), "S":(0,-1)}
+        curr = "N"
+        path = [0,0]
+        for inst in instructions:
+            if inst == "L":
+                lcount += 1
+                curr = lefts[curr]
+            elif inst == "R":
+                rcount += 1
+                curr = rights[curr]
+            else:
+                path[0]+= go[curr][0]
+                path[1]+= go[curr][1]
+            
+        if path == [0,0] or curr != "N":
+                return True
+        return False
+                
