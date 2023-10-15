9b1bdd84da49145ac0b4a8776d83c4752bc7115b
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-15 13:58:06 +0300

    Time: 842 ms (42.30%), Space: 16.5 MB (51.78%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -1,25 +1,20 @@
 class Solution:
-    def __init__(self):
-        self.dicts = defaultdict(int)
-        self.ans = 0
     def numTeams(self, rating: List[int]) -> int:
-        self.dfs(0,rating,rating[0] > rating[1])
-        return self.ans
-    def dfs(self,index,rating,flag):
-        
-        if (index,flag) in self.dicts:
-            return self.dicts[(index,flag)]
-        
-        for val in range(index+1, len(rating)):
-            if rating[index] > rating[val]:
-                result = self.dfs(val,rating,True)
-                self.dicts[(index,True)] += 1
-            else:
-                result = self.dfs(val,rating,False)
-                self.dicts[(index,False)] += 1
+        size = len(rating)
+        dp =[[1]*(2) for _ in range(size)]
+        count = 0
+        for index in range(size-2,-1,-1):
+            for val in range(index+1,size):
+                if rating[index] > rating[val]:
+                    result = dp[val][0]
+                    dp[index][0] += 1
+                else:
+                    result = dp[val][1]
+                    dp[index][1] += 1
+                if result >= 2:
+                    count += (result -1)
+   
+            
+        return count
                 
-            if result >= 2:
-                self.ans += (result-2 + 1)
-        self.dicts[(index,True)] += 1
-        self.dicts[(index,False)] += 1
-        return self.dicts[(index, flag)]
+                
