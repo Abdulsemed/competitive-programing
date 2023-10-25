22afabaa288461314cf476145daf55112e4f0d6c
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-25 17:21:15 +0300

    Time: 2595 ms (43.43%), Space: 54.9 MB (94.97%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -2,16 +2,14 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n <=2:
             return 0
-        arr = [True for i in range(n)]
+        arr = [True]*(n)
         arr[0] = arr[1] = False
         curr = 2
-        sets = set()
         while curr * curr < n:
             if arr[curr]:
                 j = curr * curr
                 while j < n:
                     arr[j] = False
-                    sets.add(j)
                     j += curr
             curr += 1
-        return n-len(sets)-2
+        return sum(arr)
