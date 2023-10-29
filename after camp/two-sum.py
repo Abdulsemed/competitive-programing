a6cf8a1d17dcc674f9c98209593e6c96e11eb540
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-10-29 14:38:12 +0300

    Added solution for Two Sum

---
+++ after camp/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for index, elt in enumerate(nums):
+            if target - elt in hash_map:
+                return [hash_map[target-elt], index]
+            hash_map[elt] = index
+        
+        
