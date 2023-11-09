ea62294067aafb9cd4c983cfa3505a74e9765d3d
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-11-09 12:35:26 +0300

    Time: 283 ms (36.07%), Space: 19.8 MB (47.87%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.py
@@ -1,22 +1,41 @@
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        duplicate = set()
         graph = defaultdict(list)
-        indegree = [0]*(n)
-        for index in range(n):
-            if leftChild[index] != -1:
-                graph[index].append(leftChild[index])
-                indegree[leftChild[index]] += 1
-            if rightChild[index] != -1:
-                graph[index].append(rightChild[index])
-                indegree[rightChild[index]] += 1
-            
-        
-        stack =[]
-        for child in range(n):
-            if indegree[child] == 0:
-                stack.append(child)
-                break
-        visited = set() if not stack else {stack[0]}
+        for child in leftChild:
+            if child != -1:
+                if child in duplicate:
+                    return False
+                duplicate.add(child)
+
+        for child in rightChild:
+            if child != -1:
+                if child in duplicate:
+                    return False
+                duplicate.add(child)
+
+
+
+        for i in range(n):
+            # self loop check
+            if i == leftChild[i] or i == rightChild[i]:
+                return False
+            if leftChild[i] != -1:
+                graph[i].append(leftChild[i])
+            if rightChild[i] != -1:
+                graph[i].append(rightChild[i])
+
+        root = []
+
+        for i in range(n):
+            if i not in duplicate:
+                root.append(i)
+
+        #only one root is assured
+        if len(root) != 1:
+            return False
+        stack = [root[0]]
+        visited = {root[0]}
         while stack:
             curr = stack.pop()
             for child in graph[curr]:
