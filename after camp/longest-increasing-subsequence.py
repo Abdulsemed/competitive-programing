b61b82d09d26366e1832ef3c232fe4173d9ccf2d
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2023-11-21 10:41:50 +0300

    Added solution for Longest Increasing Subsequence

---
+++ after camp/longest-increasing-subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        size = len(nums)
+        dp = [1]*size
+        for index in range(size-1,-1,-1):
+            for val in range(index+1,size):
+                if nums[index] < nums[val]:
+                    dp[index] = max(dp[index], dp[val]+1)
+                    
+        return max(dp)
