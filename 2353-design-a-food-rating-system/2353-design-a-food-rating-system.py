3c9fe12891ceb20c6604f4e18b9a2058bf70b797
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2023-12-18 10:18:53 +0300

    Time: 776 ms (47.93%), Space: 49.5 MB (30.77%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -3,28 +3,18 @@ class FoodRatings:
 
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
         self.dicts = {}
-        self.cuisine = defaultdict(SortedList)
+        self.cuisine = defaultdict(list)
         for index in range(len(foods)):
-            self.dicts[foods[index]] = [cuisines[index],ratings[index]]
-            self.cuisine[cuisines[index]].add([ratings[index], foods[index]])
+            self.dicts[foods[index]] = [cuisines[index],-ratings[index]]
+            heapq.heappush(self.cuisine[cuisines[index]],[-ratings[index], foods[index]])
     def changeRating(self, food: str, newRating: int) -> None:
         cuis = self.dicts[food][0]
-        rating = self.dicts[food][1]
-        self.dicts[food][1] = newRating
-        self.cuisine[cuis].remove([rating,food])
-        self.cuisine[cuis].add([newRating, food])
+        self.dicts[food][1] = -newRating
+        heapq.heappush(self.cuisine[cuis],[-newRating, food])
     def highestRated(self, cuisine: str) -> str:
-        left = 0
-        right = len(self.cuisine[cuisine])-1
-        target = self.cuisine[cuisine][-1][0]
-        while left<= right:
-            mid = left +(right-left)//2
-            if self.cuisine[cuisine][mid][0] >= target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return self.cuisine[cuisine][left][1]
-
+        while self.cuisine[cuisine][0][0] != self.dicts[self.cuisine[cuisine][0][1]][1]:
+            heapq.heappop(self.cuisine[cuisine])
+        return self.cuisine[cuisine][0][1]
 
 # Your FoodRatings object will be instantiated and called as such:
 # obj = FoodRatings(foods, cuisines, ratings)
