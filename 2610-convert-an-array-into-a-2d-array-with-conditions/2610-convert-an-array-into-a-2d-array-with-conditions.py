d0737770af44646a8a5aee3e7c1b8450e806a5ed
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-01-02 20:24:21 +0300

    Time: 105 ms (5.01%), Space: 17.3 MB (12.28%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,17 +1,13 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        graph = defaultdict(set)
-        while nums:
-            val = nums.pop()
-            flag = False
-            for index in range(len(graph)):
-                if val not in graph[index]:
-                    graph[index].add(val)
-                    flag = True
-                    break
-            if not flag:
-                graph[len(graph)].add(val)
+        dicts = {}
+        for element in nums:
+            dicts[element] = 1 + dicts.get(element, 0)
         ans = []
-        for key in graph:
-            ans.append(graph[key])
+        for key in dicts:
+            for index in range(dicts[key]):
+                if index < len(ans):
+                    ans[index].append(key)
+                else:
+                    ans.append([key])
         return ans
