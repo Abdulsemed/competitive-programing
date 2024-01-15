d6a61b9e98c09e4b38f7cd5cb69c027f50734992
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-01-15 18:50:27 +0300

    Time: 1397 ms (90.77%), Space: 73.9 MB (5.08%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,23 +1,23 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        winners ={matches[0][0]:1}
-        losers = {matches[0][1]:1}
-        size = len(matches)
-        for index in range(1,size):
-            winners[matches[index][0]] = 1 + winners.get(matches[index][0], 0)
-            losers[matches[index][1]] = 1 + losers.get(matches[index][1], 0)
-            
-        winnerList =[]
-        for winner in winners:
-            if winner in losers:
-                continue
-            else:
-                winnerList.append(winner)
-        loserList = []      
-        for loser in losers:
-            if losers[loser] > 1:
-                continue
-            else:
-                loserList.append(loser)
+        dicts = {}
+        keys = []
+        sets = set()
+        for w,l in matches:
+            dicts[l] = 1 + dicts.get(l,0)
+            if w not in sets:
+                keys.append(w)
+                sets.add(w)
                 
-        return [sorted(winnerList), sorted(loserList)]
+            if l not in sets:
+                keys.append(l)
+                sets.add(l)
+        keys.sort()
+        ans =[[],[]]    
+        for key in keys:
+            if key not in dicts:
+                ans[0].append(key)
+            elif dicts[key] == 1:
+                ans[1].append(key)
+        
+        return ans
