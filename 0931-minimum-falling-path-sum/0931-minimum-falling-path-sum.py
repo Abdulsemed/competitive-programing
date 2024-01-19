a1b801cb5607208feeb85eef5ee4940281f54f88
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-01-19 18:15:40 +0300

    Time: 137 ms (50.54%), Space: 17.5 MB (66.98%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -3,24 +3,18 @@ class Solution:
         return 0 <= row < n and 0<= col < n
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         direction = [(1,-1),(1,0),(1,1)]
-        minim = float("inf")
-        dicts = {}
-        for index in range(len(matrix)-1,-1,-1):
+        dicts = matrix.copy()
+        for index in range(len(matrix)-2,-1,-1):
             for val in range(len(matrix)-1,-1,-1):
-                if index == len(matrix)-1:
-                    dicts[(index,val)] = matrix[index][val]
-                else:
-                    curr = float("inf")
-                    for _r,_c in direction:
-                        new_r = index + _r
-                        new_c = val + _c
-                        if self.inbound(new_r,new_c,len(matrix)):
-                            curr = min(curr, dicts[(new_r,new_c)])
-                    dicts[(index,val)] = curr + matrix[index][val]
-                if index == 0:
-                    minim = min(minim, dicts[(index,val)])
-
-        return minim
+                curr = float("inf")
+                for _r,_c in direction:
+                    new_r = index + _r
+                    new_c = val + _c
+                    if self.inbound(new_r,new_c,len(matrix)):
+                        curr = min(curr, dicts[new_r][new_c])
+                dicts[index][val] = curr + matrix[index][val]
+                    
+        return min(dicts[0])
                 
                 
             
