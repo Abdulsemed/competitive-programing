da97ab0f8964d620737f4ee99442e275d03e3dd5
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-01-22 12:44:56 +0300

    Time: 170 ms (57.50%), Space: 18.1 MB (77.07%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,21 +1,13 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        index = 0
-        size = len(nums)
-        arr = []
-        while index < size:
-            pos = nums[index]-1
-            if nums[index] == nums[pos] and pos != index:
-                if nums[index] not in arr:
-                    arr.append(nums[index])
-                index += 1
-            elif pos != index:
-                nums[pos], nums[index] = nums[index], nums[pos]
-            else:
-                index += 1
+        sets = Counter(nums)
+        maxim = max(nums)
+        ans = [0,0]
+        for val in range(1,maxim+2):
+            if val in sets and sets[val] > 1:
+                ans[0] = val
+            elif val not in sets:
+                ans[1] = val
+            if ans[0] != 0 and ans[1] !=0: break
                 
-        for index in range(size):
-            if nums[index] != index +1:
-                arr.append(index+1)
-                break
-        return arr
+        return ans
