0388ccbc3d0bb3ebec1eff7358ef78cae8709b9b
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-02-18 20:09:13 +0300

    Time: 944 ms (7.06%), Space: 18.1 MB (10.85%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,8 +1,6 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         dicts = {}
-        ans = 0
-        glob  = 0
         nums.sort()
         def dfs(index):
             nonlocal glob, ans
@@ -21,10 +19,13 @@ class Solution:
                     maxim = curr.copy()
             
             dicts[index] = maxim +[nums[index]]
-            if len(dicts[index]) > glob:
-                glob = len(dicts[index])
-                ans = index
             return dicts[index]
         
         dfs(0)
+        ans = 0
+        glob = 0
+        for key in dicts:
+            if len(dicts[key]) > glob:
+                glob = len(dicts[key])
+                ans = key
         return dicts[ans]
