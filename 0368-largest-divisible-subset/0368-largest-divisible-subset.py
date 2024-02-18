7db0f2beb3e62dcd2cf951de6b7a029ad0ba3f1b
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-02-18 20:17:30 +0300

    Time: 234 ms (34.61%), Space: 16.9 MB (42.08%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,31 +1,21 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        dicts = {}
         nums.sort()
-        def dfs(index):
-            nonlocal glob, ans
-            if index >= len(nums):
-                return []
-            
-            if (index) in dicts:
-                return dicts[index]
-            maxim = []
-            for val in range(index+1,len(nums)):
-                holder = dfs(val) 
-                curr = []
-                if nums[val] % nums[index] == 0:
-                    curr = holder
-                if len(curr) > len(maxim):
-                    maxim = curr.copy()
-            
-            dicts[index] = maxim +[nums[index]]
-            return dicts[index]
-        
-        dfs(0)
+        dp = []
+        maxim = 0
         ans = 0
-        glob = 0
-        for key in dicts:
-            if len(dicts[key]) > glob:
-                glob = len(dicts[key])
-                ans = key
-        return dicts[ans]
+        
+        for index in range(len(nums)):
+            dp.append([nums[index]])
+            
+        for index in range(len(nums)-1,-1,-1):
+            for val in range(index+1,len(nums)):
+                if nums[val] % nums[index] == 0:
+                    if len(dp[val]) > len(dp[index])-1:
+                        dp[index] = [nums[index]] + dp[val] 
+            if len(dp[index]) > maxim:
+                maxim = len(dp[index])
+                ans = index
+                
+        return dp[ans]
+        
