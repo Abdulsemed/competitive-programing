a97bba64661e47aca520cbf2056aac35047dad3f
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-02-21 13:37:44 +0300

    Time: 41 ms (94.18%), Space: 16.6 MB (81.69%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,18 +1,12 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        curr = 2**31
-        ans = 0
-        while curr:
-            if curr & right != 0 and curr & left == 0:
-                return ans
-            elif curr & right == 0 and curr & left != 0:
-                return ans
-            elif curr & right != 0 and  curr & left != 0:
-                ans += curr
-                flag = False
-            curr = curr >> 1
-        
-        return ans
+        shifts  = 0
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shifts += 1
+            
+        return left << shifts
             
         
         
