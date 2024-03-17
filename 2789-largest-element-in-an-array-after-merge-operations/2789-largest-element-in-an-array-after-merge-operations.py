738cde65d7b4ae3029a5aa20e6a861e4caa5f451
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-03-17 11:50:23 +0300

    Time: 724 ms (55.08%), Space: 31 MB (44.07%) - LeetHub

--- 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.py
+++ 2789-largest-element-in-an-array-after-merge-operations/2789-largest-element-in-an-array-after-merge-operations.py
@@ -2,8 +2,9 @@ class Solution:
     def maxArrayValue(self, arr: List[int]) -> int:
         maxim = arr[-1]
         for index in range(len(arr)-1,0,-1):
-            if arr[index] >= arr[index-1]:
-                arr[index-1] += arr[index]
-            maxim = max(maxim, arr[index-1])
+            if maxim >= arr[index-1]:
+                maxim += arr[index-1]
+            else:
+                maxim = arr[index-1]
             
         return maxim
