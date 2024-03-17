a0a2987bfd5b623d5fa9fda5380fad3ad60bbb62
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-03-17 11:35:36 +0300

    Time: 114 ms (11.96%), Space: 17 MB (12.92%) - LeetHub

--- 1764-form-array-by-concatenating-subarrays-of-another-array/1764-form-array-by-concatenating-subarrays-of-another-array.py
+++ 1764-form-array-by-concatenating-subarrays-of-another-array/1764-form-array-by-concatenating-subarrays-of-another-array.py
@@ -1,6 +1,10 @@
 class Solution:
     def canChoose(self, groups: List[List[int]], nums: List[int]) -> bool:
         curr = 0
+        size = 0
+        for index in range(len(groups)):
+            size += len(groups[index])
+        
         for index in range(len(groups)):
             while curr < len(nums):
                 idx = 0
@@ -12,6 +16,9 @@ class Solution:
                     curr += idx
                     break
                 curr += 1
+            size -= idx
+            if len(nums)-curr < size:
+                return False
         
         return False
                 
