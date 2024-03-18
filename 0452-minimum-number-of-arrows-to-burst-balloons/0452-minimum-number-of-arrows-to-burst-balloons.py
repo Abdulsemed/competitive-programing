aa23e29861eb07d973eb49690ba500e4e8a38062
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-03-18 10:06:29 +0300

    Time: 1133 ms (23.26%), Space: 62.7 MB (80.82%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -1,35 +1,13 @@
 class Solution:
-    def find(self,node):
-        if self.dicts[node] != node:
-            self.dicts[node] = self.find(self.dicts[node])
-        return self.dicts[node]
-    
-    def union(self, node1, node2):
-        rep1 = self.find(node1)
-        rep2 = self.find(node2)
-        size1 = self.size[rep1]
-        size2 = self.size[rep2]
-        if rep1 == rep2:
-            return 
-        elif size1[0] <= size2[0] <= size1[1]:
-            self.size[rep1] =(size1[0], min(size2[1],size1[1]))
-            self.dicts[rep2] = rep1 
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        self.dicts = {}
-        self.size = {}
         points.sort(key = lambda x:(x[0],x[1]))
+        curr= points[0]
+        count = 1
         for index in range(len(points)):
-            points[index] = tuple(points[index])
-            self.dicts[points[index]] = points[index]
-            self.size[points[index]] = points[index]
-            
-        for index in range(len(points)-1):
-            self.union(points[index], points[index+1])
-        
-        count = 0
-        visited = set()
-        for point in points:
-            if self.find(point) == point and point not in visited:
+            if curr[0] <= points[index][0] <= curr[1]:
+                curr = [curr[0], min(points[index][1], curr[1])]
+            else:
                 count += 1
-            visited.add(point)
+                curr = points[index]
+                
         return count
