bac2ebcc7a5180b46f2f2cb7c27b2e8fb0c42962
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2024-03-28 10:17:58 +0300

    Time: 502 ms (64.10%), Space: 19.2 MB (78.43%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -4,35 +4,15 @@ class Solution:
         left = 0
         right = 0
         count = 0
-        lastL = 0
-        lastR = 0
         if k == 0:
             return 0
-        while left < len(nums):
-            if curr < k:
-                if right < len(nums):
-                    curr *= nums[right]
-                    right += 1
-                    
-                else:
-                    val = lastR-lastL
-                    count -= (val*(val+1))//2
-                    val = (right-left)
-                    count += (val*(val+1))//2
-                    break
-            elif left == right:
-                right += 1
-            else:
-                val = lastR-lastL
-                count -= (val*(val+1))//2
-                val = (right-1-left)
-                count += (val*(val+1))//2
-                while left < right and curr >= k:
-                    curr /= nums[left]
-                    left += 1
-                lastL = left
-                lastR = right-1
-                
+        while right < len(nums):
+            curr *= nums[right]
+            right += 1  
+            while left < right and curr >= k:
+                curr /= nums[left]
+                left += 1
+            count += (right-left)
                     
         return count
             
