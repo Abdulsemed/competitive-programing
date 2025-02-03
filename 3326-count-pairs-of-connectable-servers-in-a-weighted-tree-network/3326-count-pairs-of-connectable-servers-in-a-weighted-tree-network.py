0a4e241eb5816dbfe4c31b89af3c9e3c5ab5c691
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2025-02-03 17:09:08 +0300

    Time: 760 ms (83.49%), Space: 203.2 MB (6.8%) - LeetHub

--- 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
+++ 3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network/3326-count-pairs-of-connectable-servers-in-a-weighted-tree-network.py
@@ -1,48 +1,34 @@
 class Solution:
     def countPairsOfConnectableServers(self, edges: List[List[int]], signalSpeed: int) -> List[int]:
-        adj = defaultdict(list)
-        for u, v, w in edges:
-            adj[u].append((v, w))
-            adj[v].append((u, w))
+        @cache
+        def dfs(n, parent):
+            temp = []
+            for element,weight in dicts[n]:
+                if element != parent:
+                    weights = dfs(element, n)
+                    if n != parent:
+                        for w in weights:
+                            temp.append(w+weight)
+                    else:
+                        tempCount = 0
+                        for w in weights:
+                            if (w + weight) % signalSpeed == 0:
+                                tempCount += 1
+                        temp.append(tempCount)
+                else:
+                    temp.append(0)
+            return temp
 
-        # DFS function to calculate the number of servers in the subtree
-        # whose distance from the root is divisible by signalSpeed
-        def dfs(node, parent, distance, signalSpeed, adj, count):
-            if distance % signalSpeed == 0:
-                count[node] = 1
-            else:
-                count[node] = 0
+        dicts = defaultdict(list)
+        for src,end,weight in edges:
+            dicts[src].append([end, weight])
+            dicts[end].append([src,weight])
+        ans = [0]*(len(edges)+1)
+        for index in range(len(ans)):
+            temp = dfs(index, index)
+            sums = sum(temp)
+            for element in temp:
+                ans[index] += ((sums- element)*element)
+            ans[index] = ans[index]//2
 
-            for neighbor, weight in adj[node]:
-                if neighbor == parent:
-                    continue
-                dfs(neighbor, node, distance + weight, signalSpeed, adj, count)
-                count[node] += count[neighbor]
-
-        # Function to calculate the number of connectable pairs for a given root
-        def calculate_pairs(root, adj, signalSpeed):
-            count = {}
-            dfs(root, -1, 0, signalSpeed, adj, count)
-
-            total = 0
-            S = count[root] - 1  # Exclude the root itself
-            subtree_counts = []
-            for neighbor, _ in adj[root]:
-                if neighbor in count:
-                    subtree_counts.append(count[neighbor])
-
-            # Calculate the number of valid pairs
-            for i in range(len(subtree_counts)):
-                for j in range(i + 1, len(subtree_counts)):
-                    total += subtree_counts[i] * subtree_counts[j]
-
-            return total
-
-        # Calculate the result for each server
-        result = []
-        n = len(edges)+1
-        for c in range(n):
-            pairs = calculate_pairs(c, adj, signalSpeed)
-            result.append(pairs)
-
-        return result
+        return ans
