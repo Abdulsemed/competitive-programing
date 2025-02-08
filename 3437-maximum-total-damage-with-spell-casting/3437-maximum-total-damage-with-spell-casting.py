fa24e87dfaa212773265ee4c67e5b495a9453654
Abdulsemed Muhaba <88775958+Abdulsemed@users.noreply.github.com>
2025-02-08 10:59:16 +0300

    Time: 660 ms (34.55%), Space: 159.3 MB (19.41%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.py
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.py
@@ -1,27 +1,28 @@
 class Solution:
     def maximumTotalDamage(self, power: List[int]) -> int:
-        def dfs(index,before):
+        def dfs(index):
             if index >= len(power):
                 return 0
 
-            if index == before+6:
-                return power[index] * dicts[power[index]]
+            if (power[index]) in memo:
+                return memo[power[index]]
 
-            if (index, before) in memo:
-                return memo[(index, before)]
+            if power[index] - power[index-1] > 2:
+                val = memo[power[index-1]] + power[index] * dicts[power[index]]
+            elif power[index] - power[index-2] > 2:
+                val = memo[power[index-2]] + power[index] * dicts[power[index]]
+            else:
+                val = memo[power[index-3]] + power[index] * dicts[power[index]]
 
-            val1 = 0
-            if power[index] > power[before]+2:
-                val1 = dfs(index+1, index) + (power[index] * dicts[power[index]])
-            val2 = dfs(index+1,before)
-
-            memo[(index, before)] = max(val1, val2)
-            return memo[(index, before)]
+            memo[power[index]] = max(val,memo[power[index-1]])
+            curr = dfs(index+1)
+            return memo[power[index]]
 
         dicts = {}
         for element in power:
             dicts[element] = 1 + dicts.get(element, 0)
         
-        power = [-2] + sorted(list(dicts.keys()))
-        memo = {}
-        return dfs(1,0)
+        power = [0,0,0] + sorted(list(dicts.keys()))
+        memo = {0:0}
+        dfs(3)
+        return memo[power[-1]]
